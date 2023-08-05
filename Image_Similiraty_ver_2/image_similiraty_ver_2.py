d59ad60820b251034c1ef7c6326e12ecfb0c9a32
5hseok <ohhs1010@gmail.com>
2023-08-05 15:32:22 +0900

    feat : 이미지 표현 및 csv 파일 생성 이미지와 유사도를 cv2로 나타나게 했고 csv파일에 이미지 링크와 유사도를 정리하여 파일을 생성했다.

--- Image_Similiraty_ver_2/image_similiraty_ver_2.py
+++ Image_Similiraty_ver_2/image_similiraty_ver_2.py
@@ -50,18 +50,64 @@ image_pairs = [
 
 # 각 이미지 쌍의 코사인 유사도를 저장할 리스트를 초기화합니다
 similarities = []
+import cv2
+import urllib.request
+from matplotlib import pyplot as plt
+import numpy as np
 
-# 이미지 쌍별 코사인 유사도를 계산합니다
 for pair in image_pairs:
     source_embedding = predict(pair[0])
     target_embedding = predict(pair[1])
     similarity = cos_sim(source_embedding, target_embedding)
     similarities.append(similarity)
-    if round(similarity*100,4)!=100.0000:
-        print("Similarity between image pair: {:.20f}%".format(similarity*100))
-    else:
-        print("Similarity between image pair: 100%")
+
+    plt.figure(figsize=(10, 5)) # 적절한 크기를 지정할 수 있습니다.
+
+    # 첫 번째 이미지와 유사도 값을 출력합니다
+    with urllib.request.urlopen(pair[0]) as url:
+        image_data = url.read()
+    image_array = np.asarray(bytearray(image_data), dtype=np.uint8)
+    image = cv2.imdecode(image_array, cv2.IMREAD_COLOR)
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    
+    plt.subplot(1, 2, 1) # 1행 2열의 subplot에서 첫 번째 위치에 이미지를 넣습니다
+    plt.title("Image 1")
+    plt.imshow(image)
+    plt.axis('off')
+
+    # 두 번째 이미지와 유사도 값을 출력합니다
+    with urllib.request.urlopen(pair[1]) as url:
+        image_data = url.read()
+    image_array = np.asarray(bytearray(image_data), dtype=np.uint8)
+    image = cv2.imdecode(image_array, cv2.IMREAD_COLOR)
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    
+    plt.subplot(1, 2, 2) # 1행 2열의 subplot에서 두 번째 위치에 이미지를 넣습니다
+    plt.title(f"Image 2 (Similarity: {similarity * 100:.2f}%)")
+    plt.imshow(image)
+    plt.axis('off')
+
+    # 이미지와 함께 유사도 값을 출력합니다
+    plt.show()
+
 
 # 유사도의 평균을 계산합니다
 average_similarity = sum(similarities) / len(similarities)
 print("Average similarity: {:.4f}%".format(round(average_similarity * 100,4)))
+
+import csv
+# CSV 파일을 쓰기 모드로 연다.
+with open('image_pairs_similarity.csv', mode='w', newline='') as csv_file:
+    csv_writer = csv.writer(csv_file)
+    csv_writer.writerow(['Source Image', 'Target Image', 'Similarity'])
+
+    for pair in image_pairs:
+        source_embedding = predict(pair[0])
+        target_embedding = predict(pair[1])
+        similarity = cos_sim(source_embedding, target_embedding)
+        similarities.append(similarity)
+
+        # 유사도 값을 CSV 파일에 기록한다.
+        csv_writer.writerow([pair[0], pair[1], similarity])
+
+
