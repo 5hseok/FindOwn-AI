7e76ef179c4cc8cd2c1acce820680de63e0db871
5hseok <ohhs1010@gmail.com>
2023-08-09 18:38:49 +0900

    이미지를 전체 DB에서 하나씩 유사도 찾는 모델

--- Image_Search/Image_Search_NN.py
+++ Image_Search/Image_Search_NN.py
@@ -11,8 +11,8 @@ def create_feature_extractor(model, return_nodes=None):
 
     return return_nodes_output
 
-model = EfficientNet.from_pretrained('efficientnet-b7')
-model_features = create_feature_extractor(model,return_nodes={'_conv_head':'_conv_head'})
+model = EfficientNet.from_pretrained('efficientnet-b0')
+model_features = create_feature_extractor(model,return_nodes={'avgpool':'avgpool'})
 model.eval()    
 
 import requests
@@ -69,7 +69,7 @@ def predict(image_path):
     return image_feature.detach().numpy()
     
 
-root_dir = "C:\\Users\\DGU_ICE\\FindOwn\\Image"
+root_dir = "C:\\Users\\sam\\Desktop\\user\\Pictures\\Pattern_Image"
 #자신의 로컬에 있는 Image 파일 주소로 설정할 것.
 
 import os
@@ -77,7 +77,7 @@ import re
 from collections import defaultdict
 
 # 타겟 이미지 경로
-target_image_path = "C:\\Users\\DGU_ICE\\FindOwn\\Image\\adidas_1.jpg"
+target_image_path = "C:\\Users\\sam\\Desktop\\user\\Pictures\\Pattern_Image\\1.jpg"
 
 # 디렉토리에서 이미지 파일들 찾기
 image_files = []
@@ -102,34 +102,38 @@ for image_path in image_files:
     if source_embedding is None:
         print(f"Skipping {image_path} due to prediction error.")
         continue
+    if target_embedding is None:
+        print(f"Skipping {image_path} due to prediction error.")
+        continue
     similarity = cos_sim(source_embedding, target_embedding)
     similarities.append(similarity)
 
     print("Similarity between", image_path, "and target image:", similarity)
-
-    # 이미지와 유사도 출력
-    plt.figure(figsize=(10, 5)) # 적절한 크기를 지정
-
-    # 첫 번째 이미지와 유사도 값 출력
-    image = cv2.imread(image_path)
-    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-
-    plt.subplot(1, 2, 1) # 1행 2열의 subplot에서 첫 번째 위치에 이미지를 넣습니다
-    plt.title("Image 1")
-    plt.imshow(image)
-    plt.axis('off')
-
-    # 두 번째 이미지와 유사도 값을 출력합니다
-    image = cv2.imread(target_image_path)
-    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
     
-    plt.subplot(1, 2, 2) # 1행 2열의 subplot에서 두 번째 위치에 이미지를 넣습니다
-    plt.title(f"Target Image (Similarity: {similarity * 100:.2f}%)")
-    plt.imshow(image)
-    plt.axis('off')
+    # if similarity > 0.8:
+    #     # 이미지와 유사도 출력
+    #     plt.figure(figsize=(10, 5)) # 적절한 크기를 지정
 
-    # 이미지와 함께 유사도 값을 출력합니다
-    plt.show()
+    #     # 첫 번째 이미지와 유사도 값 출력
+    #     image = cv2.imread(image_path)
+    #     image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+    #     plt.subplot(1, 2, 1) # 1행 2열의 subplot에서 첫 번째 위치에 이미지를 넣습니다
+    #     plt.title("Image 1")
+    #     plt.imshow(image)
+    #     plt.axis('off')
+
+    #     # 두 번째 이미지와 유사도 값을 출력합니다
+    #     image = cv2.imread(target_image_path)
+    #     image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        
+    #     plt.subplot(1, 2, 2) # 1행 2열의 subplot에서 두 번째 위치에 이미지를 넣습니다
+    #     plt.title(f"Target Image (Similarity: {similarity * 100:.2f}%)")
+    #     plt.imshow(image)
+    #     plt.axis('off')
+
+    #     # 이미지와 함께 유사도 값을 출력합니다
+    #     plt.show()
 
 # 유사도의 평균을 계산합니다
 average_similarity = sum(similarities) / len(similarities)
