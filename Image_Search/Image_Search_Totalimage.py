a25b8f59e943bb4032518c37e4e664df3070c97f
5hseok <ohhs1010@gmail.com>
2023-08-18 03:24:32 +0900

    feat : 가장 높은 이미지 유사도 이미지 출력 root_dir에서 가장 유사한 이미지 10개를 출력하고 피클파일로 저장

--- Image_Search/Image_Search_Totalimage.py
+++ Image_Search/Image_Search_Totalimage.py
@@ -69,7 +69,7 @@ def predict(image_path):
     return image_feature.detach().numpy()
     
 
-root_dir = "C:\\Users\\sam\\Desktop\\user\\Pictures\\Pattern_Image"
+root_dir = "C:\\Users\\DGU_ICE\\FindOwn\\ImageDB\\Logos"
 #자신의 로컬에 있는 Image 파일 주소로 설정할 것.
 
 import os
@@ -96,6 +96,8 @@ import numpy as np
 # 타겟 이미지 특징 추출
 target_embedding = predict(target_image_path)
 
+results = []
+
 # 각 이미지와 타겟 이미지의 유사도 계산
 for image_path in image_files:
     source_embedding = predict(image_path)
@@ -107,51 +109,60 @@ for image_path in image_files:
         continue
     similarity = cos_sim(source_embedding, target_embedding)
     similarities.append(similarity)
-
-    print("Similarity between", image_path, "and target image:", similarity)
     
-    # if similarity > 0.8:
-    #     # 이미지와 유사도 출력
-    #     plt.figure(figsize=(10, 5)) # 적절한 크기를 지정
+    results.append((image_path,similarity))
+    
+top_results = sorted(results, key=lambda x: x[1],reverse=True)[:10]
 
-    #     # 첫 번째 이미지와 유사도 값 출력
-    #     image = cv2.imread(image_path)
-    #     image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+for image_path, similarity in top_results:
+    print("Similarity between", image_path, "and target image:",similarity)
 
-    #     plt.subplot(1, 2, 1) # 1행 2열의 subplot에서 첫 번째 위치에 이미지를 넣습니다
-    #     plt.title("Image 1")
-    #     plt.imshow(image)
-    #     plt.axis('off')
-
-    #     # 두 번째 이미지와 유사도 값을 출력합니다
-    #     image = cv2.imread(target_image_path)
-    #     image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-        
-    #     plt.subplot(1, 2, 2) # 1행 2열의 subplot에서 두 번째 위치에 이미지를 넣습니다
-    #     plt.title(f"Target Image (Similarity: {similarity * 100:.2f}%)")
-    #     plt.imshow(image)
-    #     plt.axis('off')
-
-    #     # 이미지와 함께 유사도 값을 출력합니다
-    #     plt.show()
 
 # 유사도의 평균을 계산합니다
 average_similarity = sum(similarities) / len(similarities)
 print("Average similarity: {:.4f}%".format(round(average_similarity * 100,4)))
 
-import csv
-# CSV 파일을 쓰기 모드로 연다.
-with open('image_similarity_Totalimage.csv', mode='w', newline='') as csv_file:
-    csv_writer = csv.writer(csv_file)
-    csv_writer.writerow(['Image', 'Target Image', 'Similarity'])
+# import csv
+# # CSV 파일을 쓰기 모드로 연다.
+# with open('image_similarity_Top10image.csv', mode='w', newline='') as csv_file:
+#     csv_writer = csv.writer(csv_file)
+#     csv_writer.writerow(['Image', 'Target Image', 'Similarity'])
 
-    for image_path in image_files:
-        source_embedding = predict(image_path)
-        if source_embedding is None:
-            print(f"Skipping {image_path} due to prediction error.")
-            continue
-        similarity = cos_sim(source_embedding, target_embedding)
-        similarities.append(similarity)
+#     for image_path, similarity in top_results:
+#         # 유사도 값을 CSV 파일에 기록한다.
+#         csv_writer.writerow([image_path, target_image_path, similarity * 100])
+import pickle
 
-        # 유사도 값을 CSV 파일에 기록한다.
-        csv_writer.writerow([image_path, target_image_path, similarity*100])
+# 피클 파일로 저장
+with open('image_similarity_Top10image.pkl', 'wb') as pickle_file:
+    pickle.dump(top_results, pickle_file)
+    
+# 피클 파일 불러오기
+with open('image_similarity_Top10image.pkl', 'rb') as pickle_file:
+    loaded_results = pickle.load(pickle_file)
+
+    for image_path, similarity in loaded_results:
+    # 이미지와 유사도 출력
+        plt.figure(figsize=(10, 5)) # 적절한 크기를 지정
+
+        # 첫 번째 이미지와 유사도 값 출력
+        image = cv2.imread(image_path)
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+        plt.subplot(1, 2, 1) # 1행 1열의 subplot에서 첫 번째 위치에 이미지를 넣습니다
+        plt.title(f"Image 1 Similarity: {similarity * 100:.2f}%")
+        plt.imshow(image)
+        plt.axis('off')
+        
+        image=cv2.imread(target_image_path)
+        image=cv2.cvtColor(image,cv2.COLOR_BGR2RGB)
+        plt.subplot(1,2,2)
+        plt.title("Target Image")
+        plt.imshow(image)
+        plt.axis('off')
+        
+        plt.show()
+        
+# # 불러온 결과 출력
+# for image_path, similarity in loaded_results:
+#     print("Similarity between", image_path, "and target image:", similarity)
