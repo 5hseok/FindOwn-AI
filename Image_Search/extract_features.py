9a2668f510bb045ceed680cace65fcda2e9f335e
5hseok <ohhs1010@gmail.com>
2023-10-21 02:21:42 +0900

    Feat : kipris에서 불러온 이미지 데이터들도 pkl로 만들기 위한 코드로 수정

--- Image_Search/extract_features.py
+++ Image_Search/extract_features.py
@@ -6,13 +6,13 @@ import pickle
 def main():
     # Initialize the model.
     similar_model = models.Image_Search_Model("C:\\Users\\DGU_ICE\\FindOwn\\ImageDB\\Logos")
-
+    similar_model1 = models.Image_Search_Model("C:\\Users\\DGU_ICE\\AI_Trademark_IMG")
     # Extract features from all images.
-    similar_model.extract_features()
+    combined_features = similar_model.extract_features() + similar_model1.extract_features()
 
     # Save the features to a file.
     with open('features.pkl', 'wb') as f:
-        pickle.dump(similar_model.features, f)
+        pickle.dump(combined_features, f)
 
 if __name__ == '__main__':
     main()
