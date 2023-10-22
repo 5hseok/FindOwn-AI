51a95bfbc44a719f5fba41b11d427b81f1569f7e
5hseok <ohhs1010@gmail.com>
2023-10-23 02:00:18 +0900

    Refactor : extract_feaures 형 변환 list로 변환해 generator 형식을 list로 바꿈

--- Image_Search/extract_features.py
+++ Image_Search/extract_features.py
@@ -8,7 +8,7 @@ def main():
     similar_model = models.Image_Search_Model("C:\\Users\\DGU_ICE\\FindOwn\\ImageDB\\Logos")
     similar_model1 = models.Image_Search_Model("C:\\Users\\DGU_ICE\\AI_Trademark_IMG")
     # Extract features from all images.
-    combined_features = similar_model.extract_features() + similar_model1.extract_features()
+    combined_features = list(similar_model.extract_features()) + list(similar_model1.extract_features())
     # Save the features to a file.
     with open('features.pkl', 'wb') as f:
         pickle.dump(combined_features, f)
