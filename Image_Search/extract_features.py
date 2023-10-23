dfa1b78435c1f5eb4a133f6f5b4ee0923bb96218
5hseok <ohhs1010@gmail.com>
2023-10-23 15:44:07 +0900

    Style : features 결합 방식 변경 반복문으로 변경

--- Image_Search/extract_features.py
+++ Image_Search/extract_features.py
@@ -5,10 +5,18 @@ import pickle
 
 def main():
     # Initialize the model.
-    similar_model = models.Image_Search_Model("C:\\Users\\DGU_ICE\\FindOwn\\ImageDB\\Logos")
-    similar_model1 = models.Image_Search_Model("C:\\Users\\DGU_ICE\\AI_Trademark_IMG")
+    similar_model = models.Image_Search_Model("C:\\Users\\DGU_ICE\\FindOwn\\ImageDB\\Logos", "check1.pkl")
+    similar_model1 = models.Image_Search_Model("C:\\Users\\DGU_ICE\\AI_Trademark_IMG","ckeck2.pkl")
+
     # Extract features from all images.
-    combined_features = list(similar_model.extract_features()) + list(similar_model1.extract_features())
+    combined_features = []
+    
+    for feature in similar_model.extract_features():
+        combined_features.append(feature)
+
+    for feature in similar_model1.extract_features():
+        combined_features.append(feature)
+
     # Save the features to a file.
     with open('features.pkl', 'wb') as f:
         pickle.dump(combined_features, f)
