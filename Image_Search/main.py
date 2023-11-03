8cb8950e7febf5fbe313ac0fd1e4ca582a1d9bb6
5hseok <ohhs1010@gmail.com>
2023-11-03 23:02:17 +0900

    Refactor : 이미지 출력 코드 삭제

--- Image_Search/main.py
+++ Image_Search/main.py
@@ -2,74 +2,48 @@ from pydantic import BaseModel
 import models
 import cv2
 import pickle
+import os
 import matplotlib.pyplot as plt
 from PIL import Image
-# class ImageSearchRequest(BaseModel):
-#     target_image_path: str
-#     top10_image_list: list[str]
 
-# app = FastAPI()
 # Initialize the models.
-target_image_path = "C:\\Users\\DGU_ICE\\FindOwn\\ImageDB\\KakaoTalk_20230216_133749847.png"
-#Test image
+# url을 받아오는 걸로 변경 요망
+################################################################################################################
 target_image_path = "https://trademark.help-me.kr/images/blog/trademark-registration-all-inclusive/image-05.png"
-similar_results = {}
-
+################################################################################################################
+root_dir = "C:\\Users\\DGU_ICE\\FindOwn\\ImageDB\\Logos"
+#target_image_path를 url로 받아오면 아래 코드로 유사도 검사 후 결과 dict를 json으로 만들어 다시 전송
+similar_results_dict = {}
+image_list = []
 with open('features_logo.pkl','rb') as f:
     load = pickle.load(f)
 for image_path, array in load:
-    similar_results.update({image_path:0})
+    similar_results_dict.update({image_path:0.0})
+    image_list.append(image_path)
 similar_model = models.Image_Search_Model(pre_extracted_features='features_logo.pkl')
-top_10_image_list = similar_model.search_similar_images(target_image_path)
-for image_path, accuracy in top_10_image_list:
-    similar_results[image_path] = accuracy
-print(similar_results)
-# for similar_accuracy in top_10_image_list:
-#     similar_results.append(similar_accuracy * 0.6)
-    
-root_dir = 'C:\\Users\\DGU_ICE\\FindOwn\\ImageDB\\Logos'
-plt.figure(figsize=(20, 20))   # 플롯 크기 설정
-for i, (img_path, sim) in enumerate(top_10_image_list):
-    img = Image.open(img_path)    # 이미지 불러오기
-    plt.subplot(2, 5, i+1)        # 2행 5열의 서브플롯 생성
-    plt.imshow(img)               # 이미지 출력
-    plt.title(f"Rank {i+1}: Similaritysimilar {sim}")    # 이미지 제목 설정
-    plt.axis('off')               # 축 레이블 제거
-
-plt.tight_layout()    # 플롯 간격 조절
-plt.show()   
+efficientnet_image_list = similar_model.search_similar_images(target_image_path)
+for image_path, accuracy in efficientnet_image_list:
+    similar_results_dict[image_path] = 0.6*accuracy
+  
     
 color_model = models.ColorSimilarityModel()
+if not os.path.exists('colorHistograms_logo.pkl'):
+    color_model.save_histograms(root_dir,'colorHistograms_logo.pkl')
 histograms = color_model.load_histograms('colorHistograms_logo.pkl')
 similarities = color_model.predict(target_image_path, histograms)
-top_10_images = similarities[:10]
-print(top_10_images)
-# for color_similarity in top_10_images:
-    
-plt.figure(figsize=(20, 20))   # 플롯 크기 설정
-for i, (img_path, sim) in enumerate(top_10_images):
-    img = Image.open(str(root_dir+'\\'+img_path))    # 이미지 불러오기
-    plt.subplot(2, 5, i+1)        # 2행 5열의 서브플롯 생성
-    plt.imshow(img)               # 이미지 출력
-    plt.title(f"Rank {i+1}: SimilarityColor {sim}")    # 이미지 제목 설정
-    plt.axis('off')               # 축 레이블 제거
+color_dicision_images = similarities
 
-plt.tight_layout()    # 플롯 간격 조절
-plt.show()            # 플롯 보여주기 
+for img_path, color_accuracy in color_dicision_images:
+    similar_results_dict[img_path] += 0.3 * color_accuracy
 
-Object_model  = models.Image_Object_Detections()
-result = Object_model.search_similar_images(target_image_path,top_10_image_list)
+
+Object_model  = models.Image_Object_Detections(len(image_list))
+result = Object_model.search_similar_images(target_image_path,image_list)
 print(result)
-plt.figure(figsize=(20, 20))   # 플롯 크기 설정
-for i, (img_path, _ ,sim) in enumerate(result):
-    img = Image.open(str(img_path))    # 이미지 불러오기
-    plt.subplot(2, 5, i+1)        # 2행 5열의 서브플롯 생성
-    plt.imshow(img)               # 이미지 출력
-    plt.title(f"Rank {i+1}: SimilarityObject {sim}")    # 이미지 제목 설정
-    plt.axis('off')               # 축 레이블 제거
+for img_path, object_accuracy in result:
+    similar_results_dict[img_path] += 0.1 * object_accuracy
+print(similar_results_dict.sort())
 
-plt.tight_layout()    # 플롯 간격 조절
-plt.show() 
 
 
 
