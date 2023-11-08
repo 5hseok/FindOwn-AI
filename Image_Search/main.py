0692d8dd3ff455670792f552d588ded541e04d1f
5hseok <ohhs1010@gmail.com>
2023-11-09 02:26:21 +0900

    가중치 업데이트 및 json 기능 구현

--- Image_Search/main.py
+++ Image_Search/main.py
@@ -7,16 +7,22 @@ import os
 import matplotlib.pyplot as plt
 from PIL import Image
 import matplotlib.image as mpimg
+import requests
+import json
+
 def min_max_normalize(scores):
     min_score = min(scores)
     max_score = max(scores)
+    if max_score == min_score:
+        return [1.0 for _ in scores]
     return [(score - min_score) / (max_score - min_score) for score in scores]
 # Initialize the models.
 # url을 받아오는 걸로 변경 요망
 ################################################################################################################
 target_image_path= "https://trademark.help-me.kr/images/blog/trademark-registration-all-inclusive/image-05.png"
+target_image_path = "C:\\Users\\DGU_ICE\\FindOwn\\ImageDB\\fakestar.png"
 #빽다방
-# target_image_path = "https://upload.wikimedia.org/wikipedia/ko/thumb/3/33/%ED%86%A0%ED%8A%B8%EB%84%98_%ED%99%8B%EC%8A%A4%ED%8D%BC_FC_%EB%A1%9C%EA%B3%A0.svg/800px-%ED%86%A0%ED%8A%B8%EB%84%98_%ED%99%8B%EC%8A%A4%ED%8D%BC_FC_%EB%A1%9C%EA%B3%A0.svg.png"
+target_image_path = "https://upload.wikimedia.org/wikipedia/ko/thumb/3/33/%ED%86%A0%ED%8A%B8%EB%84%98_%ED%99%8B%EC%8A%A4%ED%8D%BC_FC_%EB%A1%9C%EA%B3%A0.svg/800px-%ED%86%A0%ED%8A%B8%EB%84%98_%ED%99%8B%EC%8A%A4%ED%8D%BC_FC_%EB%A1%9C%EA%B3%A0.svg.png"
 # #토트넘
 # target_image_path = "https://scontent-gmp1-1.xx.fbcdn.net/v/t39.30808-1/362637775_114168721749133_6181487638898434694_n.jpg?stp=cp0_dst-jpg_e15_p120x120_q65&_nc_cat=110&ccb=1-7&_nc_sid=5f2048&_nc_ohc=jmClhneOLzoAX8jTvls&_nc_ht=scontent-gmp1-1.xx&oh=00_AfCVJOHkeT9DFmpRTNrXp7lXqZTXkUy03MJ6U9YywHMcJg&oe=654A97FD"
 # #백소정
@@ -36,14 +42,15 @@ with open('features_logo.pkl','rb') as f:
 for image_path, array in load:
     similar_results_dict.update({image_path:0.0})
 
+#EfficientNet_results
 similar_model = models.Image_Search_Model(pre_extracted_features='features_logo.pkl')
 efficientnet_image_list = similar_model.search_similar_images(target_image_path,len(similar_results_dict))
 efficientnet_scores = [accuracy for img_path, accuracy in efficientnet_image_list]
 efficientnet_scores = min_max_normalize(efficientnet_scores)
 for (image_path, _), score in zip(efficientnet_image_list,efficientnet_scores):
-    similar_results_dict[image_path] += 0.6 * score
+    similar_results_dict[image_path] += 0.7 * score
   
-    
+# color Histogram_result
 color_model = models.ColorSimilarityModel()
 if not os.path.exists('colorHistograms_logo.pkl'):
     color_model.save_histograms(root_dir,'colorHistograms_logo.pkl')
@@ -52,22 +59,23 @@ similarities = color_model.predict(target_image_path, histograms)
 color_scores = [accuracy for img_path, accuracy in similarities]
 color_scores = min_max_normalize(color_scores)
 for (image_path, _), score in zip(similarities,color_scores):
-    similar_results_dict[image_path] += 0.1 * score
+    similar_results_dict[image_path] -= 1.0 * score
 
     
-    
+# object_detection_retinanet_result
 Object_model  = models.Image_Object_Detections(len(similar_results_dict))
 if not os.path.exists('object_logo.pkl'):
     Object_model.create_object_detection_pkl(root_dir,'object_logo.pkl')
 with open('object_logo.pkl','rb') as f:
     detection_dict = pickle.load(f)
 result = Object_model.search_similar_images(target_image_path,detection_dict)
-object_scores = [accuracy for img_path, _, accuracy in result]
-object_scores = min_max_normalize(object_scores)
-for (img_path, _, _),score in zip(result, object_scores):
-    similar_results_dict[img_path] += 0.1 * score
+if len(result) != 0:
+    object_scores = [accuracy for img_path, _, accuracy in result]
+    object_scores = min_max_normalize(object_scores)
+    for (img_path, _, _),score in zip(result, object_scores):
+        similar_results_dict[img_path] += 0.05 * score
 
-
+#resnet_results
 cnn = models.CNNModel()
 if not os.path.exists('cnn_features.pkl'):
     cnn.extract_features_from_dir(root_dir, 'cnn_features.pkl')
@@ -76,7 +84,7 @@ cnn_scores = [accuracy for img_path, accuracy in cnn_similarities]
 cnn_scores = min_max_normalize(cnn_scores)
 for (img_path, _ ), score in zip(cnn_similarities,cnn_scores):
     img_path = root_dir+'\\'+img_path
-    similar_results_dict[img_path] += 0.2 * score
+    similar_results_dict[img_path] += 2.5 * score
     
 similar_results_dict = sorted(similar_results_dict.items(), key=lambda x: x[1], reverse=True)
 
@@ -92,8 +100,7 @@ fig, ax = plt.subplots(1, N+1, figsize=(20, 10))
 # Display target image
 if target_image_path.startswith('http://') or target_image_path.startswith('https://'):
     with urllib.request.urlopen(target_image_path) as url:
-        img = Image.open(url).convert('RGB')
-        img = np.array(img)
+        img = Image.open(url)
 else:
     img = mpimg.imread(target_image_path)
 ax[0].imshow(img)
@@ -113,3 +120,41 @@ for i in range(1, N+1):
 
 plt.tight_layout()
 plt.show()
+
+
+#Test#
+img1_path = "C:\\Users\\DGU_ICE\\FindOwn\\ImageDB\\loading.png"
+img2_path = "C:\\Users\\DGU_ICE\\FindOwn\\ImageDB\\invaild_img.png"
+
+testmodel = models.Image_Search_Model()
+testmodel.test(img1_path, img2_path)
+
+####                                Sending json to server                                ####
+
+# Create a list of dictionaries, each containing the image path and accuracy
+top_results = similar_results_dict[:N]
+results_list = [{"image_path": img_path, "accuracy": accuracy} for img_path, accuracy in top_results]
+
+# Convert the list to JSON
+results_json = json.dumps(results_list)
+
+# Specify the URL to send the POST request to
+url = 'http://example.com/api'  # Change this
+
+# Set the headers for the request
+headers = {'Content-Type': 'application/json'}
+
+# Send the POST request
+response = requests.post(url, headers=headers, data=results_json)
+
+# Check the response
+if response.status_code == 200:
+    print('Successfully sent the POST request.')
+else:
+    print(f'Failed to send the POST request. Status code: {response.status_code}')
+    
+    
+    
+# Show json
+data = json.loads(results_json)
+print(data)
