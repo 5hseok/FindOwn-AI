15c5435d2d060e1c1d25a58b71d3ca528f0362a1
5hseok <ohhs1010@gmail.com>
2023-11-13 16:54:00 +0900

    Test : 새로운 로고 이미지 추가 및 코드 변경

--- Image_Search/main.py
+++ Image_Search/main.py
@@ -32,23 +32,23 @@ target_image_path= "https://trademark.help-me.kr/images/blog/trademark-registrat
 # target_image_path = "C:\\Users\\DGU_ICE\\FindOwn\\ImageDB\\fakecapa.png"
 
 ################################################################################################################
-root_dir = "C:\\Users\\DGU_ICE\\FindOwn\\ImageDB\\Logos"
+root_dir = "C:\\Users\\DGU_ICE\\FindOwn\\extracted_images"
 #target_image_path를 url로 받아오면 아래 코드로 유사도 검사 후 결과 dict를 json으로 만들어 다시 전송
 similar_results_dict = {}
 
 if not os.path.exists('features_logo.pkl'):
     similar_model = models.Image_Search_Model()
     Trademark_pkl = similar_model.extract_features(root_dir)
-    with open('features_logo.pkl','wb') as f:
+    with open('features_logo_test.pkl','wb') as f:
         pickle.dump(list(Trademark_pkl),f)    
     
-with open('features_logo.pkl','rb') as f:
+with open('features_logo_test.pkl','rb') as f:
     load = pickle.load(f)
 for image_path, array in load:
     similar_results_dict.update({image_path:0.0})
 
 #EfficientNet_results
-similar_model = models.Image_Search_Model(pre_extracted_features='features_logo.pkl')
+similar_model = models.Image_Search_Model(pre_extracted_features='features_logo_test.pkl')
 efficientnet_image_list = similar_model.search_similar_images(target_image_path,len(similar_results_dict))
 efficientnet_scores = [accuracy for img_path, accuracy in efficientnet_image_list]
 efficientnet_scores = min_max_normalize(efficientnet_scores)
@@ -57,9 +57,9 @@ for (image_path, _), score in zip(efficientnet_image_list,efficientnet_scores):
   
 # color Histogram_result
 color_model = models.ColorSimilarityModel()
-if not os.path.exists('colorHistograms_logo.pkl'):
-    color_model.save_histograms(root_dir,'colorHistograms_logo.pkl')
-histograms = color_model.load_histograms('colorHistograms_logo.pkl')
+if not os.path.exists('colorHistograms_logo_test.pkl'):
+    color_model.save_histograms(root_dir,'colorHistograms_logo_test.pkl')
+histograms = color_model.load_histograms('colorHistograms_logo_test.pkl')
 similarities = color_model.predict(target_image_path, histograms)
 color_scores = [accuracy for img_path, accuracy in similarities]
 color_scores = min_max_normalize(color_scores)
@@ -69,9 +69,9 @@ for (image_path, _), score in zip(similarities,color_scores):
     
 # object_detection_retinanet_result
 Object_model  = models.Image_Object_Detections(len(similar_results_dict))
-if not os.path.exists('object_logo.pkl'):
-    Object_model.create_object_detection_pkl(root_dir,'object_logo.pkl')
-with open('object_logo.pkl','rb') as f:
+if not os.path.exists('object_logo_test.pkl'):
+    Object_model.create_object_detection_pkl(root_dir,'object_logo_test.pkl')
+with open('object_logo_test.pkl','rb') as f:
     detection_dict = pickle.load(f)
 result = Object_model.search_similar_images(target_image_path,detection_dict)
 if len(result) != 0:
@@ -82,9 +82,9 @@ if len(result) != 0:
 
 #resnet_results
 cnn = models.CNNModel()
-if not os.path.exists('cnn_features.pkl'):
-    cnn.extract_features_from_dir(root_dir, 'cnn_features.pkl')
-cnn_similarities = cnn.compare_features(target_image_path, 'cnn_features.pkl')
+if not os.path.exists('cnn_features_test.pkl'):
+    cnn.extract_features_from_dir(root_dir, 'cnn_features_test.pkl')
+cnn_similarities = cnn.compare_features(target_image_path, 'cnn_features_test.pkl')
 cnn_scores = [accuracy for img_path, accuracy in cnn_similarities]
 cnn_scores = min_max_normalize(cnn_scores)
 for (img_path, _ ), score in zip(cnn_similarities,cnn_scores):
