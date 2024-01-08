93ac0571610e50ff43285023887535e54a5d5c1e
5hseok <ohhs1010@gmail.com>
2024-01-08 18:51:28 +0900

    가상환경 설정 및 프로젝트 실행

--- analysis_image/views.py
+++ analysis_image/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions, generics, status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.decorators import api_view
+from django.http import JsonResponse
+from django.views import View
+import requests
 
-# Create your views here.
+class ImageProcessView(View):
+    def post(self, request):
+        # 이미지 분석 및 Open API 요청 처리
+        image = request.FILES['image']
+        analyzed_info = self.analyze_image(image)
+        api_info = self.get_info_from_api(analyzed_info)
+        result = self.combine_info(analyzed_info, api_info)
+        
+        return JsonResponse(result)
+    
+    def analyze_image(self, image):
+        # 이미지 분석 코드 작성
+        pass
+    
+    def get_info_from_api(self, analyzed_info):
+        # Open API GET 요청 코드 작성
+        pass
+    
+    def combine_info(self, analyzed_info, api_info):
+        # 정보를 합치는 코드 작성
+        pass
