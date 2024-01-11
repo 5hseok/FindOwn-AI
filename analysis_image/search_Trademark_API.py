bfc308137f8b786c1333f6f2f731e6af5040619b
5hseok <ohhs1010@gmail.com>
2024-01-11 16:01:27 +0900

    api view

--- analysis_image/search_Trademark_API.py
+++ analysis_image/search_Trademark_API.py
@@ -15,14 +15,12 @@ def get_info_from_api(image_urls):
     # 이미지 URL 리스트를 순회
     for image_url in image_urls:
         # 출원번호 추출 (이미지 URL의 마지막 부분)
-        application_number = image_url.split('/')[-1]
+        application_number = image_url.split('\\')[-1]
         application_number = application_number.split('_')[0]
         # 요청 URL
         request_url = f"{base_url}?applicationNumber={application_number}&accessKey={access_key}"
-
         # API 요청 보내기
         response = requests.get(request_url)
-
         # 응답 확인
         if response.status_code == 200:
             # 응답을 XML 형식으로 파싱
