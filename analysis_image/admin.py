ddccbed6d08a146426b3abe06fe4b54b8cbb815d
5hseok <ohhs1010@gmail.com>
2024-01-11 10:10:48 +0900

    admin 페이지에 model 등록

--- analysis_image/admin.py
+++ analysis_image/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+from .models import ImageFeature_Eff, ImageFeature_CNN
 # Register your models here.
+admin.site.register(ImageFeature_Eff)
+admin.site.register(ImageFeature_CNN)
